79f51172796455c35dc810113064e38dc2f2979e
Davis Cook <daviscook447@gmail.com>
2019-07-20 15:27:52 -0400

    Add workshop resources to project

---
+++ src/ProfessionalAttire/DiggingAttireConfig.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using Klei.AI;
+using STRINGS;
+using UnityEngine;
+
+namespace ProfessionalAttire
+{
+    public class DiggingAttireConfig : IEquipmentConfig
+    {
+        public const string Id = "DiggingAttire";
+        public const string DisplayName = "Digger's Outfit";
+        public const string GenericName = "Clothing";
+        public const string RecipeDescription = "Lightweight and strong fibers keep this clothing from getting in a duplicant's way while digging.";
+
+        public static int DecorModifier = ClothingWearer.ClothingInfo.BASIC_CLOTHING.decorMod;
+        public static float ConductivityModifier = ClothingWearer.ClothingInfo.BASIC_CLOTHING.conductivityMod;
+        public static float HomeostasisEfficiencyModifier = ClothingWearer.ClothingInfo.BASIC_CLOTHING.homeostasisEfficiencyMultiplier;
+        public const float AttributeIncrease = ProfessionalAttirePatches.BasicAttributeIncrease;
+
+        public static readonly ClothingWearer.ClothingInfo NEW_CLOTHING =
+            new ClothingWearer.ClothingInfo(DisplayName, DecorModifier, ConductivityModifier, HomeostasisEfficiencyModifier);
+
+        public static readonly ComplexRecipe.RecipeElement[] ingredients = new ComplexRecipe.RecipeElement[]
+        {
+            new ComplexRecipe.RecipeElement("BasicFabric".ToTag(), TUNING.EQUIPMENT.VESTS.FUNKY_VEST_MASS),
+            new ComplexRecipe.RecipeElement(ElementLoader.FindElementByHash(SimHashes.Obsidian).tag, 2000.0f)
+        };
+        public static readonly ComplexRecipe.RecipeElement[] results = new ComplexRecipe.RecipeElement[]
+        {
+            new ComplexRecipe.RecipeElement(Id.ToTag(), 1f)
+        };
+
+        public static void ConfigureRecipe()
+        {
+            new ComplexRecipe(ComplexRecipeManager.MakeRecipeID("ClothingFabricator",
+                ingredients, results), ingredients, results)
+            {
+                time = TUNING.EQUIPMENT.VESTS.FUNKY_VEST_FABTIME,
+                description = RecipeDescription,
+                nameDisplay = ComplexRecipe.RecipeNameDisplay.Result,
+                fabricators = new List<Tag>() { "ClothingFabricator" },
+                sortOrder = 1
+            };
+        }
+
+        public EquipmentDef CreateEquipmentDef()
+        {
+            ClothingWearer.ClothingInfo clothingInfo = NEW_CLOTHING;
+            List<AttributeModifier> attributeModifiers = new List<AttributeModifier>();
+            EquipmentDef equipment = EquipmentTemplates.CreateEquipmentDef(
+                Id: Id,
+                Slot: TUNING.EQUIPMENT.CLOTHING.SLOT,
+                OutputElement: SimHashes.SuperInsulator,
+                Mass: TUNING.EQUIPMENT.VESTS.FUNKY_VEST_MASS,
+                Anim: "newshirticon",
+                SnapOn: TUNING.EQUIPMENT.VESTS.SNAPON0,
+                BuildOverride: "newshirt",
+                BuildOverridePriority: 4,
+                AttributeModifiers: attributeModifiers,
+                SnapOn1: TUNING.EQUIPMENT.VESTS.SNAPON1,
+                IsBody: true,
+                CollisionShape: EntityTemplates.CollisionShape.RECTANGLE,
+                width: 0.75f,
+                height: 0.4f,
+                additional_tags: null,
+                RecipeTechUnlock: null);
+            string thermalConductivityDescriptor = string.Format("{0}: {1}",
+                    DUPLICANTS.ATTRIBUTES.THERMALCONDUCTIVITYBARRIER.NAME,
+                    GameUtil.GetFormattedDistance(clothingInfo.conductivityMod));
+            equipment.additionalDescriptors.Add(new Descriptor(
+                thermalConductivityDescriptor, thermalConductivityDescriptor,
+                Descriptor.DescriptorType.Effect, false));
+            string decorDescriptor = string.Format("{0}: {1}",
+                DUPLICANTS.ATTRIBUTES.DECOR.NAME,
+                clothingInfo.decorMod);
+            equipment.additionalDescriptors.Add(
+                new Descriptor(decorDescriptor, decorDescriptor,
+                Descriptor.DescriptorType.Effect, false));
+            equipment.OnEquipCallBack = eq => CoolVestConfig.OnEquipVest(eq, clothingInfo);
+            equipment.OnUnequipCallBack = eq => CoolVestConfig.OnUnequipVest(eq);
+            equipment.RecipeDescription = RecipeDescription;
+            return equipment;
+        }
+
+        public void DoPostConfigure(GameObject go)
+        {
+            go.GetComponent<KPrefabID>().AddTag(GameTags.Clothes, false);
+            Equippable equippable = go.AddOrGet<Equippable>();
+            equippable.SetQuality(QualityLevel.Poor);
+            go.GetComponent<KBatchedAnimController>().sceneLayer = Grid.SceneLayer.BuildingBack;
+            go.GetComponent<KPrefabID>().AddTag(GameTags.PedestalDisplayable, false);
+        }
+    }
+
+}
---
+++ src/ProfessionalAttire/ProfessionalAttirePatches.cs
@@ -0,0 +1,39 @@
+﻿using Harmony;
+
+namespace ProfessionalAttire
+{
+    public class ProfessionalAttirePatches
+    {
+
+        public const float BasicAttributeIncrease = 2.0f;
+
+        [HarmonyPatch(typeof(GeneratedEquipment))]
+        [HarmonyPatch("LoadGeneratedEquipment")]
+        public class GeneratedEquipment_LoadGeneratedEquipment_Patch
+        {
+            private static void Prefix()
+            {
+                Strings.Add($"STRINGS.EQUIPMENT.PREFABS.{DiggingAttireConfig.Id.ToUpperInvariant()}.NAME", DiggingAttireConfig.DisplayName);
+                Strings.Add($"STRINGS.EQUIPMENT.PREFABS.{DiggingAttireConfig.Id.ToUpperInvariant()}.GENERICNAME", DiggingAttireConfig.GenericName);
+            }
+
+            private static void Postfix()
+            {
+                EquipmentConfigManager.Instance.RegisterEquipment(new DiggingAttireConfig());
+            }
+        }
+
+
+
+        [HarmonyPatch(typeof(ClothingFabricatorConfig))]
+        [HarmonyPatch("ConfigureRecipes")]
+        public class ClothingFabricatorConfig_ConfigureRecipes_Patch
+        {
+            private static void Postfix()
+            {
+                DiggingAttireConfig.ConfigureRecipe();
+            }
+        }
+
+    }
+}
