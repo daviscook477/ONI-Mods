52691106bb0e4c1eb89b98a5e02024c2e776ca2a
Davis Cook <daviscook447@gmail.com>
2019-07-28 03:21:51 -0400

    Add crystal water production

--- src/CrystalBiome/src/Plants/Patches.cs
+++ src/CrystalBiome/src/Plants/Patches.cs
@@ -1,4 +1,8 @@
-﻿using Harmony;
+﻿using System;
+using System.Collections.Generic;
+
+using Harmony;
+using UnityEngine;
 
 using TUNING;
 
@@ -6,6 +10,52 @@ namespace CrystalBiome.Plants
 {
     public class Patches
     {
+        public const float ConversionRate = 0.7f;
+        public const float MinDumpAmount = 1.0f;
+
+        [HarmonyPatch(typeof(PlantElementAbsorbers), nameof(PlantElementAbsorbers.Sim200ms))]
+        public class PlantElementAbsorbers_Sim200ms_Patch
+        {
+            private static void Prefix(PlantElementAbsorbers __instance, float dt)
+            {
+                List<PlantElementAbsorber> data = Traverse.Create(__instance).Field("data").GetValue<List<PlantElementAbsorber>>();
+                int count = data.Count;
+                Traverse.Create(__instance).Field("updating").SetValue(true);
+                for (int i = 0; i < count; ++i)
+                {
+                    PlantElementAbsorber plantElementAbsorber = data[i];
+                    if (plantElementAbsorber.storage != null && plantElementAbsorber.consumedElements == null)
+                    {
+                        float a = plantElementAbsorber.localInfo.massConsumptionRate * dt;
+                        PrimaryElement firstWithMass = plantElementAbsorber.storage.FindFirstWithMass(plantElementAbsorber.localInfo.tag);
+                        if (firstWithMass != null)
+                        {
+                            if (firstWithMass.Element.tag.Equals(ElementLoader.FindElementByHash(Elements.MineralWaterElement.SimHash).tag))
+                            {
+                                float amount = Mathf.Min(a, firstWithMass.Mass);
+                                float convertedAmount = amount * ConversionRate;
+                                plantElementAbsorber.storage.AddLiquid(SimHashes.Water,
+                                    convertedAmount, firstWithMass.Temperature, byte.MaxValue, 0);
+                                float availableMass = plantElementAbsorber.storage.GetMassAvailable(SimHashes.Water);
+                                Console.WriteLine(string.Format("checking available mass is {0}", availableMass));
+                                if (availableMass >= MinDumpAmount)
+                                {
+                                    PrimaryElement element = plantElementAbsorber.storage.FindFirstWithMass(ElementLoader.FindElementByHash(SimHashes.Water).tag);
+                                    Dumpable component = element.gameObject.GetComponent<Dumpable>();
+                                    if (component == null)
+                                    {
+                                        DebugUtil.LogWarningArgs("Was unable to dump water converted from mineral water!");
+                                        continue;
+                                    }
+                                    component.Dump(plantElementAbsorber.storage.transform.GetPosition());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         public const float CyclesForGrowth = 0.1f;
 
         [HarmonyPatch(typeof(EntityConfigManager), "LoadGeneratedEntities")]
