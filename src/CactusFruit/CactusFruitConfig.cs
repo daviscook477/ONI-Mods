9e6434dec0c1d349ea28b6c6b89b80a45bb380ef
Davis Cook <daviscook447@gmail.com>
2019-12-31 01:18:16 -0500

    Add cactus plant code

---
+++ src/CactusFruit/CactusFruitConfig.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using STRINGS;
+using UnityEngine;
+
+namespace CactusFruit
+{
+    public class CactusFruitConfig : IEntityConfig
+    {
+        public const string Id = "CactusFruit";
+        public const string SeedId = "CactusFruitSeed";
+
+        public static string Name = UI.FormatAsLink("Prickling Pear", Id.ToUpper());
+        public static string SeedName = UI.FormatAsLink("Prickling Pear Seed", SeedId.ToUpper());
+
+        public static string Description = $"Prickly to the touch, the {Name} produces both an edible flesh and flower.";
+        public static string SeedDescription = $"The beginnings of a {Name}. Just add {UI.FormatAsLink("Water", "WATER")}.";
+        public static string DomesticatedDescription = $"This cactus {UI.FormatAsLink("Plant", "PLANTS")} survives well in the harsh, dry desert. To thrive and flower, it needs copious amounts of {UI.FormatAsLink("Water", "WATER")}.";
+
+        public const float DefaultTemperature = 320f;
+        public const float TemperatureLethalLow = 258.15f;
+        public const float TemperatureWarningLow = 308.15f;
+        public const float TemperatureWarningHigh = 358.15f;
+        public const float TemperatureLethalHigh = 448.15f;
+
+        public const float IrrigationRate = 0.05f;
+
+        public GameObject CreatePrefab()
+        {
+            var placedEntity = EntityTemplates.CreatePlacedEntity(
+                id: Id,
+                name: Name,
+                desc: Description,
+                mass: 1f,
+                anim: Assets.GetAnim("cactusfruit_kanim"),
+                initialAnim: "idle_empty",
+                sceneLayer: Grid.SceneLayer.BuildingFront,
+                width: 1,
+                height: 1,
+                decor: TUNING.DECOR.BONUS.TIER1,
+                defaultTemperature: DefaultTemperature
+                );
+
+            EntityTemplates.ExtendEntityToBasicPlant(
+                template: placedEntity,
+                temperature_lethal_low: TemperatureLethalLow,
+                temperature_warning_low: TemperatureWarningLow,
+                temperature_warning_high: TemperatureWarningHigh,
+                temperature_lethal_high: TemperatureLethalHigh,
+                safe_elements: new[] { SimHashes.Oxygen, SimHashes.CarbonDioxide },
+                pressure_sensitive: true,
+                pressure_lethal_low: 0.0f,
+                pressure_warning_low: 0.15f,
+                crop_id: CactusFleshConfig.Id);
+
+            EntityTemplates.ExtendPlantToIrrigated(
+                template: placedEntity,
+                info: new PlantElementAbsorber.ConsumeInfo()
+                {
+                    tag = GameTags.Water,
+                    massConsumptionRate = IrrigationRate
+                });
+
+            placedEntity.AddOrGet<StandardCropPlant>();
+
+            var seed = EntityTemplates.CreateAndRegisterSeedForPlant(
+                plant: placedEntity,
+                productionType: SeedProducer.ProductionType.Harvest,
+                id: SeedId,
+                name: SeedName,
+                desc: SeedDescription,
+                anim: Assets.GetAnim("cactusseed_kanim"),
+                initialAnim: "object",
+                numberOfSeeds: 1,
+                additionalTags: new List<Tag>() { GameTags.CropSeed },
+                planterDirection: SingleEntityReceptacle.ReceptacleDirection.Top,
+                replantGroundTag: new Tag(),
+                sortOrder: 2,
+                domesticatedDescription: DomesticatedDescription,
+                collisionShape: EntityTemplates.CollisionShape.CIRCLE,
+                width: 0.2f,
+                height: 0.2f);
+
+            EntityTemplates.CreateAndRegisterPreviewForPlant(
+                seed: seed,
+                id: $"{Id}_preview",
+                anim: Assets.GetAnim("cactusfruit_kanim"),
+                initialAnim: "place",
+                width: 1,
+                height: 1);
+
+            return placedEntity;
+        }
+
+        public void OnPrefabInit(GameObject inst) { }
+
+        public void OnSpawn(GameObject inst) { }
+    }
+}
---
+++ src/CactusFruit/CactusFruitPatch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TUNING;
+using Harmony;
+
+namespace CactusFruit
+{
+    public class CactusFruitPatch
+    {
+        public const float CyclesForGrowth = 0.1f;
+
+        [HarmonyPatch(typeof(EntityConfigManager), "LoadGeneratedEntities")]
+        public class EntityConfigManager_LoadGeneratedEntities_Patch
+        {
+            private static void Prefix()
+            {
+                Strings.Add($"STRINGS.CREATURES.SPECIES.SEEDS.{CactusFruitConfig.Id.ToUpperInvariant()}.NAME", CactusFruitConfig.SeedName);
+                Strings.Add($"STRINGS.CREATURES.SPECIES.SEEDS.{CactusFruitConfig.Id.ToUpperInvariant()}.DESC", CactusFruitConfig.SeedDescription);
+
+                Strings.Add($"STRINGS.CREATURES.SPECIES.{CactusFruitConfig.Id.ToUpperInvariant()}.NAME", CactusFruitConfig.Name);
+                Strings.Add($"STRINGS.CREATURES.SPECIES.{CactusFruitConfig.Id.ToUpperInvariant()}.DESC", CactusFruitConfig.Description);
+                Strings.Add($"STRINGS.CREATURES.SPECIES.{CactusFruitConfig.Id.ToUpperInvariant()}.DOMESTICATEDDESC", CactusFruitConfig.DomesticatedDescription);
+
+                CROPS.CROP_TYPES.Add(new Crop.CropVal(CactusFleshConfig.Id, CyclesForGrowth * 600.0f, 5));
+            }
+        }
+    }
+}
