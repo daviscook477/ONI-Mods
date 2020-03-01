967d7ded82a27d8f27e56698cbabbc8a2d886347
Davis Cook <daviscook447@gmail.com>
2020-03-01 17:44:37 -0500

    Get new diet working for algae fish

--- src/PacuMorphs/AlgaePacuConfig.cs
+++ src/PacuMorphs/AlgaePacuConfig.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using STRINGS;
+using System.Collections.Generic;   
 
 namespace PacuMorphs
 {
     public class AlgaePacuConfig : IEntityConfig
     {
+        private static float KG_ORE_EATEN_PER_CYCLE = 140f;
+        private static float CALORIES_PER_KG_OF_ORE = PacuTuning.STANDARD_CALORIES_PER_CYCLE / KG_ORE_EATEN_PER_CYCLE;
+
         public static readonly EffectorValues DECOR = TUNING.BUILDINGS.DECOR.BONUS.TIER4;
         public const string ID = "PacuAlgae";
         public const string BASE_TRAIT_ID = "PacuAlgaeBaseTrait";
@@ -16,13 +20,23 @@ namespace PacuMorphs
         public static string EGG_NAME = UI.FormatAsLink("Overgrown Fry Egg", ID.ToUpper());
 
         public static GameObject CreatePacu(
-    string id,
-    string name,
-    string desc,
-    string anim_file,
-    bool is_baby)
+            string id,
+            string name,
+            string desc,
+            string anim_file,
+            bool is_baby)
         {
             GameObject wildCreature = EntityTemplates.ExtendEntityToWildCreature(BasePacuConfig.CreatePrefab(id, BASE_TRAIT_ID, name, desc, anim_file, is_baby, null, 303.15f, 353.15f), PacuTuning.PEN_SIZE_PER_CREATURE, 25f);
+            Diet diet = new Diet(new Diet.Info[1]
+            {
+              new Diet.Info(new HashSet<Tag>()
+              {
+                SimHashes.Phosphorite.CreateTag(),
+                SimHashes.Fertilizer.CreateTag(),
+              }, SimHashes.Algae.CreateTag(), CALORIES_PER_KG_OF_ORE, TUNING.CREATURES.CONVERSION_EFFICIENCY.NORMAL, null, 0.0f, false, false)
+            });
+            wildCreature.GetDef<CreatureCalorieMonitor.Def>().diet = diet;
+            wildCreature.GetDef<SolidConsumerMonitor.Def>().diet = diet;
             return wildCreature;
         }
 
